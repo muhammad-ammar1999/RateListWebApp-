7a2ff8ad4e83e13ae686bff6d0b422da96059bc4
1999MuhmmadAmmar <muhammad.ammar@ciphertronix.com>
2021-09-10 17:41:55 +0500

     DevExpress Datagrid ASP.NET Core Controls concepts implementation

---
+++ ShopMs/Controllers/ButtonController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopMs.Controllers
+{
+    public class ButtonController : Controller
+    {
+        public ActionResult PredefinedTypes()
+        {
+            return View();
+        }
+    }
+}
